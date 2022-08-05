054f71abb1c2e4080c00bd88862cf0ce7154689c
sepeca15 <sepeca15@gmail.com>
2022-08-05 00:19:51 +0000

    app terminado

--- src/app.py
+++ src/app.py
@@ -1,4 +1,50 @@
-from utils import db_connect
-engine = db_connect()
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
+import seaborn as sns
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+import pickle
 
-# your code here
+
+df = pd.read_csv('/workspace/Logistic-Regression-Project-Your-first-ML-Algorithm/data/processed/df_processed.csv')
+
+features = ['age_group', 'education', 'month', 'duration',
+       'campaign', 'previous', 'cons.price.idx', 'cons.conf.idx',
+       'nr.employed', 'job_admin.', 'job_blue-collar', 'job_entrepreneur',
+       'job_housemaid', 'job_management', 'job_retired', 'job_self-employed',
+       'job_services', 'job_student', 'job_technician', 'job_unemployed',
+       'marital_divorced', 'marital_married', 'marital_single', 'default_no',
+       'default_yes', 'housing_no', 'housing_yes', 'loan_no', 'loan_yes',
+       'contact_cellular', 'contact_telephone', 'poutcome_failure',
+       'poutcome_nonexistent', 'poutcome_success']
+
+X = df[features]
+y = df["y"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, test_size=0.2, random_state=42)
+
+clf2 = LogisticRegression(max_iter=1000)
+solvers = ['newton-cg', 'lbfgs', 'liblinear']
+penalty = ['l2']
+c_values = [100, 10, 1.0, 0.1, 0.01]
+
+grid = dict(solver=solvers,penalty=penalty,C=c_values)
+cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+grid_search = GridSearchCV(estimator=clf2, param_grid=grid, n_jobs=-1, cv=cv, scoring='accuracy',error_score=0)
+grid_result = grid_search.fit(X, y)
+
+params_op = grid_result.best_params_
+clf_op = grid_result.best_estimator_
+clf_op.fit(X_train,y_train)
+
+filename = '/workspace/Logistic-Regression-Project-Your-first-ML-Algorithm/models/model_logisticReg.pickle'
+pickle.dump(clf_op, open(filename, 'wb'))
